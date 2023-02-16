d87aa581d18a7478b693b7b59ddd0279eab03972
Vebjørn Ostnes Gaupset <55185409+vgaupset@users.noreply.github.com>
2023-02-16 01:45:53 +0000

    WIP

--- pdffuns.py
+++ pdffuns.py
@@ -17,17 +17,26 @@ def norm1D(my,Sgm,x):
 
 def norm2D(my,Sgm,x1,x2):
     X1,X2 = np.meshgrid(x1,x2)
-    [n,d]=np.shape(X1)
+    [n, d]=np.shape(X1)
     p = np.zeros(np.shape(X1))
     for i in np.arange(0, n):
         for j in np.arange(0,d):
             xij = np.array([[X1[i,j]],[X2[i,j]]])
             dist = xij - my
-            distT = np.transpose(dist)
-            
+            distT = np.transpose(dist) 
             denominator = 2 * np.pi * np.sqrt(np.linalg.det(Sgm))
             exponent = - 1/2 * distT @ np.linalg.inv(Sgm) @ dist
             p[i][j] = 1 / denominator * np.exp(exponent)
 
     return p,X1,X2
     
+
+def knn2D(pw, k, my, X):
+    D = []
+    n = X.shape[1]
+    for i in range(n):
+        D.append(np.linalg.norm(np.array([X[0][i] - my[0], X[1][i] - my[1] ])))
+    D.sort()
+    dk = D[k-1]
+    Pk = k*pw/(np.pi*pow(dk,2)*n)
+    return Pk
